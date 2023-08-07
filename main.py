e1eaedfeef440014aee4487f18f7b87e8114ea0a
perintyler <perintyler@gmail.com>
2023-08-07 16:51:39 -0400

    use iso stamps for the saved models' filenames

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import time
 import json
 import numpy as np
+from datetime import datetime
 import matplotlib.pyplot as plt
 import torch
 
@@ -16,32 +17,39 @@ from dalle2_pytorch import (
   OpenAIClipAdapter
 )
 
-TEXT_INPUT = 'road to nowhere'
+# ----------------------------------------------------------------
 
-MODEL_EXISTS = False
+TEXT_INPUT = 'road'
 
-NET1_STATE_FILEPATH = "./models/nn1.pth"
+# ----------------------------------------------------------------
 
-NET2_STATE_FILEPATH = "./models/nn2.pth"
+USE_PRETRAINED_MODEL = False
 
 NUM_TRAINING_ITERATIONS = 20
+
+START_TIME = datetime.now()
+
+NET1_STATE_FILEPATH = f"./models/UNET1-{START_TIME.isoformat()}.pth"
+
+NET2_STATE_FILEPATH = f"./models/UNET2-{START_TIME.isoformat()}.pth"
+
+# ----------------------------------------------------------------
 
 VERBOSE = True
 
 LOGFILE_NAME = f'./logs/{time.time()}.json'
 
-# ----------------------------------------------------------------
-# ----------------------------------------------------------------
-
 logs = []
 
+# --------------------------------------------------------------------------------------------------------------------------------
+# <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+# --------------------------------------------------------------------------------------------------------------------------------
+
+# dummy data
 images = torch.randn(4, 3, 256, 256)
 text = torch.randint(0, 49408, (4, 256))
 
-# openai pretrained clip - defaults to ViT-B/32
-clip = OpenAIClipAdapter()
-
-# prior networks (with transformer)
+clip = OpenAIClipAdapter() # openai pretrained clip - defaults to ViT-B/32
 
 prior_network = DiffusionPriorNetwork(
   dim = 512,
@@ -93,8 +101,9 @@ decoder = Decoder(
   text_cond_drop_prob = 0.5
 )
 
-# ----------------------------------------------------------------
-# ----------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------------------
+# <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+# --------------------------------------------------------------------------------------------------------------------------------
 
 def log(msg):
   """prints if verbose mode is on, otherwise gets written to log file"""
@@ -108,6 +117,33 @@ def print_divider():
   print('[]'*16)
   print('-'*32)
   print('')
+
+def save_state_to_json():
+  """"""
+  print('<> saving unet states to JSON files <>')
+  for i in (0,1):
+    model = decoder.unets[i]
+    state = {paramTensor: model.state_dict()[paramTensor].size() for paramTensor in model.state_dict()}
+    with open(f'nn{i+1}.json', 'w') as nnStateJSONFile: 
+      json.dump(state, nnStateJSONFile)
+
+def save_models():
+  """"""
+  print('<> saving unet models to PTH files <>')
+  log(f'saving models to {NET1_STATE_FILEPATH} and {NET2_STATE_FILEPATH}')
+  if not os.path.isdir('models'): os.mkdir('models')
+  torch.save(unet1.state_dict(), NET1_STATE_FILEPATH)
+  torch.save(unet2.state_dict(), NET2_STATE_FILEPATH)
+
+def save_logs():
+  print(f'<> saving logs to {LOGFILE_NAME} <>')
+  if not os.path.isdir('logs'): os.mkdir('logs')
+  with open(LOGFILE_NAME, 'w') as logfile:
+    json.dump(logs, logfile)
+
+# --------------------------------------------------------------------------------------------------------------------------------
+# <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+# --------------------------------------------------------------------------------------------------------------------------------
 
 def train():
   """"""
@@ -132,31 +168,10 @@ def train():
 
   train_diffision_model()
   train_unets()
-
-def save_state_to_json():
-  """"""
-  print('<> saving unet states to JSON files <>')
-  for i in (0,1):
-    model = decoder.unets[i]
-    state = {paramTensor: model.state_dict()[paramTensor].size() for paramTensor in model.state_dict()}
-    with open(f'nn{i+1}.json', 'w') as nnStateJSONFile: 
-      json.dump(state, nnStateJSONFile)
-
-def save_models():
-  """"""
-  print('<> saving unet models to PTH files <>')
-  log(f'saving models to {NET1_STATE_FILEPATH} and {NET2_STATE_FILEPATH}')
-  torch.save(unet1.state_dict(), NET1_STATE_FILEPATH)
-  torch.save(unet2.state_dict(), NET2_STATE_FILEPATH)
-
-def save_logs():
-  print(f'<> saving logs to {LOGFILE_NAME} <>')
-  with open(LOGFILE_NAME, 'w') as logfile:
-    json.dump(logs, logfile)
+  print_divider()
 
 def generate_images(saveToFile=True):
   """"""
-  print_divider()
   print(f'<> generating images for input: "{TEXT_INPUT}" <>')
   dalle2 = DALLE2(
     prior = diffusion_prior,
@@ -170,8 +185,12 @@ def generate_images(saveToFile=True):
   )
 
   log(f'successfully generated {len(images)} images')
-
+  print_divider()
   print(f'<> saving images to PNG files <>')
+
+  if not os.path.isdir('images'): 
+    os.mkdir('images')
+
   if saveToFile: # save your image (in this example, of size 256x256)
     numImages = len(list(filter(lambda fn: fn.endswith('.png'), os.listdir('images'))))
     for image, i in enumerate(images):
@@ -182,10 +201,14 @@ def generate_images(saveToFile=True):
 
   return images
 
-if __name__ == '__main__':
+# --------------------------------------------------------------------------------------------------------------------------------
+# <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+# --------------------------------------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__': 
   train()
   save_models()
   generate_images()
   save_logs()
-
+  log(f'finished in {START_TIME - datetime.now()} seconds')
 
