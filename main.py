0a958a7f257402e46b59bd7e5105049a8b9f933f
perintyler <perintyler@gmail.com>
2023-08-07 16:32:18 -0400

    add some logging functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """main.py"""
 
 import os
+import time
 import json
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,12 +24,16 @@ NET1_STATE_FILEPATH = "./models/nn1.pth"
 
 NET2_STATE_FILEPATH = "./models/nn2.pth"
 
-NUM_TRAINING_ITERATIONS = 100
+NUM_TRAINING_ITERATIONS = 20
 
 VERBOSE = True
 
+LOGFILE_NAME = f'./logs/{time.time()}.json'
+
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
+
+logs = []
 
 images = torch.randn(4, 3, 256, 256)
 text = torch.randint(0, 49408, (4, 256))
@@ -91,31 +96,46 @@ decoder = Decoder(
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
 
-def vprint(msg):
-  """verbose printing"""
+def log(msg):
+  """prints if verbose mode is on, otherwise gets written to log file"""
+  logs.append(message)
   if (VERBOSE):
-    print(msg)
+    print(f'\t - {msg}')
+
+def print_divider():
+  print('')
+  print('-'*32)
+  print('[]'*16)
+  print('-'*32)
+  print('')
 
 def train():
   """"""
   def train_diffision_model():
-    vprint('training diffusion model')
+    print('<> training diffusion model <>')
+    startTime = time.time()
     for i in range(NUM_TRAINING_ITERATIONS):
       loss = diffusion_prior(text, images)
       loss.backward()
+      log(f'loss after iteration #{i+1}: {loss}')
+    log(f'DONE -- trained diffusion model in {time.time() - startTime} seconds')
 
   def train_unets():
-    vprint('training unets')
+    print('<> training unets <>')
+    startTime = time.time()
     for i in range(NUM_TRAINING_ITERATIONS):
       for unet_number in (1, 2):
         loss = decoder(images, text = text, unet_number = unet_number) # this can optionally be decoder(images, text) if you wish to condition on the text encodings as well, though it was hinted in the paper it didn't do much
         loss.backward()
+        log(f'Loss after iteration {i+1} for #{unet_number} = {loss}')
+    log(f'DONE -- trained unets in {time.time() - startTime} seconds')
 
   train_diffision_model()
   train_unets()
 
 def save_state_to_json():
   """"""
+  print('<> saving unet states to JSON files <>')
   for i in (0,1):
     model = decoder.unets[i]
     state = {paramTensor: model.state_dict()[paramTensor].size() for paramTensor in model.state_dict()}
@@ -124,13 +144,20 @@ def save_state_to_json():
 
 def save_models():
   """"""
-  vprint(f'saving models to {NET1_STATE_FILEPATH} and {NET2_STATE_FILEPATH}')
+  print('<> saving unet models to PTH files <>')
+  log(f'saving models to {NET1_STATE_FILEPATH} and {NET2_STATE_FILEPATH}')
   torch.save(unet1.state_dict(), NET1_STATE_FILEPATH)
   torch.save(unet2.state_dict(), NET2_STATE_FILEPATH)
 
+def save_logs():
+  print(f'<> saving logs to {LOGFILE_NAME} <>')
+  with open(LOGFILE_NAME, 'w') as logfile:
+    json.dump(logs, logfile)
+
 def generate_images(saveToFile=True):
   """"""
-  vprint(f'generating images for input: {TEXT_INPUT}')
+  print_divider()
+  print(f'<> generating images for input: "{TEXT_INPUT}" <>')
   dalle2 = DALLE2(
     prior = diffusion_prior,
     decoder = decoder
@@ -142,13 +169,16 @@ def generate_images(saveToFile=True):
     return_pil_images = True
   )
 
-  vprint(f'Successfully generated {len(images)} images')
+  log(f'successfully generated {len(images)} images')
 
+  print(f'<> saving images to PNG files <>')
   if saveToFile: # save your image (in this example, of size 256x256)
     numImages = len(list(filter(lambda fn: fn.endswith('.png'), os.listdir('images'))))
     for image, i in enumerate(images):
       imageNumber = numImages + i + 1
-      plt.imsave(f"images/dalle2-output-{imageNumber}.png", image) # finally save your prediction .
+      imageFileName = f"images/dalle2-output-{imageNumber}.png"
+      plt.imsave(imageFileName, image) # finally save your prediction .
+      log(f'saved image #{imageNumber} to {imageFileName}')
 
   return images
 
@@ -156,6 +186,6 @@ if __name__ == '__main__':
   train()
   save_models()
   generate_images()
-  # print(f'\n\nLOSS = {loss}\n\n')
+  save_logs()
 
 
