767f632b06bd4d9e357695c896117f02c7c2fee2
perintyler <perintyler@gmail.com>
2023-08-10 18:01:27 -0400

    implement saving/loading `Lyrics` objects to/from JSON

--- lyrics.py
+++ lyrics.py
@@ -1,65 +1,119 @@
 """subtitles.py"""
 
 import pathlib
+import json
 import whisper
 import librosa
 import soundfile as sf
 from datetime import timedelta
 from typing import List, Dict
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 
 @dataclass
 class Bar:
   """a timestamped line of lyrics"""
+
   text: str
-  timestamp: float
+  line_number: int
+  start_time: float
+  end_time: float
+
+  def as_dict(self):
+    return {
+      'text': self.text,
+      'line_number': self.line_number,
+      'start_time': self.start_time,
+      'end_time': self.end_time,
+    }
+
+  @classmethod
+  def from_json(Cls, json_bar):
+    """
+    ...
+    """
+    text = json_bar['text']
+    line_number = json_bar['line_number'] if 'line_number' in json_bar else json_bar['id']+1
+    start_time = json_bar['start_time'] if 'start_time' in json_bar else json_bar['start']
+    end_time = json_bar['end_time'] if 'end_time' in json_bar else json_bar['end']
+    return Cls(text.strip(), line_number, start_time, end_time)
 
 @dataclass
 class Lyrics:
   """audio transcriptions with timestamps"""
 
-  segments: List[Dict] 
-
-  def get_bars(self) -> List[Bar]:
-    """..."""
-    bars = []
-    for segment in self.segments:
-      line_of_lyrics = segment['text'].strip()
-      timestamp = segment['start']
-      bars.append(Bar(line_of_lyrics, timestamp))
-    return bars
+  bars: List[Bar]
+  source_file: str
 
   def get_plaintext(self) -> str:
     """
     returns all lyrics as a single string
     """ 
-    return '\n'.join(bar.text for bar in self.get_bars())
+    return '\n'.join(bar.text for bar in self.bars)
 
   def save_as_srt(self, outfile):
     """
     saves each bar and its timestamp and saves it to a SRT file (standard transcription format)
     """
-    formatted_segments = []
-    for segment in self.segments:
-      startTime = str(0) + str(timedelta(seconds=int(segment['start']))) + ',000'
-      endTime = str(0) + str(timedelta(seconds=int(segment['end']))) + ',000'
-      text = segment['text']
-      segmentId = segment['id']+1
-      segment = f"{segmentId}\n{startTime} --> {endTime}\n{text[1:] if text[0] == ' ' else text}"
-      formatted_segments.append(segment)
-
     assert outfile.endswith('.srt')
+
+    srt_sections = []
+
+    for bar in self.bars:
+      start_time_string = f'0{timedelta(seconds=int(bar.start_time))},000'
+      end_time_string = f'0{timedelta(seconds=int(bar.end_time))},000'
+      srt_sections.append(f"{bar.line_number}\n{start_time_string} --> {end_time_string}\n{bar.text.strip()}")
+
+    srt_file_contents = '\n\n'.join(srt_sections)
+
     with open(outfile, 'w', encoding='utf-8') as srt_file:
-      srt_file_contents = '\n\n'.join(formatted_segments)
       srt_file.write(srt_file_contents)
 
-    return formatted_segments
+    return srt_file_contents
+
+  def as_dict(self) -> dict:
+    """
+    serializes all encapsulated data, which can be used to re-create instances of `Lyrics` later on
+    """
+    return {
+      'bars': [bar.as_dict() for bar in self.bars],
+      'source_file': self.source_file, 
+    }
+
+  def save_as_json(self, outfile):
+    """
+    writes all encapsulated data to a JSON file, which can be loaded from file later on with `load_from_json`
+    """
+    assert outfile.endswith('.json')
+    json_contents = self.as_dict()
+
+    with open(outfile, 'w') as lyrics_json_file:
+      json.dump(json_contents, lyrics_json_file)
+
+    return json_contents
 
   @classmethod
-  def load(Cls, filepath: pathlib.Path):
+  def load_from_json(Cls, filepath):
     """
-    uses the whisper library to generate timestamped lyrics from audio 
+    creates a new instance of `Lyrics` using a JSON file created by the `save_as_json` function
     """
+    with open(filepath, 'r') as lyrics_json_file:
+      json_contents = json.load(lyrics_json_file)
+
+    source_file = json_contents['source_file']
+    bars = [Bar.from_json(bar_json) for bar_json in json_contents['bars']]
+
+    return Cls(bars, source_file)
+
+  @classmethod
+  def load_from_audio(Cls, filepath):
+    """
+    creates a new instance of `Lyrics` by transcribing lyrics with OpenAI's whisper library 
+    """
+    source_file = filepath
     model = whisper.load_model("base")
-    segments = model.transcribe(audio=filepath, fp16=False)['segments']
-    return Cls(segments)
+
+    transcription = model.transcribe(audio=filepath, fp16=False)
+    bars = [Bar.from_json(segment) for segment in transcription['segments']]
+
+    return Cls(bars, source_file)
+
--- lyrics_test.py
+++ lyrics_test.py
@@ -4,10 +4,10 @@ import pathlib
 import os
 from lyrics import Lyrics
 
-lyrics1 = Lyrics.load('test-data/audio/something-beatles_verse1.wav')
+lyrics1 = Lyrics.load_from_audio('test-data/audio/something-beatles_verse1.wav')
 
 def test_get_bars():
-  assert [bar.text for bar in lyrics1.get_bars()] == [
+  assert [bar.text for bar in lyrics1.bars] == [
     "Something in the way she moves attracts me like no other lover",
     "Something in the way she moves me"
   ]
@@ -30,3 +30,15 @@ Something in the way she moves attracts me like no other lover
 2
 00:00:19,000 --> 00:00:26,000
 Something in the way she moves me"""
+
+def test_save_and_load_lyrics_from_json():
+  json_file_path = '/tmp/lyrics-save-and-load-test.json'
+  lyrics1.save_as_json(json_file_path)
+  loaded_lyrics = Lyrics.load_from_json(json_file_path)
+  assert lyrics1.source_file == loaded_lyrics.source_file
+  for original_bar, loaded_bar in zip(lyrics1.bars, loaded_lyrics.bars):
+    assert original_bar.text == loaded_bar.text
+    assert original_bar.line_number == loaded_bar.line_number
+    assert original_bar.start_time == loaded_bar.start_time
+    assert original_bar.end_time == loaded_bar.end_time
+
