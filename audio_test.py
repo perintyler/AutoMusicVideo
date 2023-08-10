21a0f4952bff85848cff25f8467af9e9954070f4
perintyler <perintyler@gmail.com>
2023-08-10 14:00:10 -0400

    test Audio::filepath property

--- audio_test.py
+++ audio_test.py
@@ -3,10 +3,12 @@ from audio import Audio
 import numpy as np
 
 def test_load_audio():
-  audio = Audio.load('test-data/audio/something-beatles_verse1.wav')
+  path_to_test_file = 'test-data/audio/something-beatles_verse1.wav'
+  audio = Audio.load(path_to_test_file)
   assert audio.waveform is not None
   assert type(audio.waveform) is np.ndarray
   assert len(audio.waveform) > 0
   assert audio.samplerate is not None
   assert type(audio.samplerate) is int
   assert audio.samplerate > 0
+  assert audio.filepath == path_to_test_file
