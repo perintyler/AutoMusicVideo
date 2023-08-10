11fd8ea18ba1b4044cd841e2b47f086bf6362472
perintyler <perintyler@gmail.com>
2023-08-10 15:04:11 -0400

    function for attaching audio to a video

--- stitch.py
+++ stitch.py
@@ -3,6 +3,7 @@
 import librosa
 
 import moviepy.editor as mp
+from audio import Audio
 
 def create_video_from_gifs(infiles, outfile = None, gif_duration = None) -> mp.VideoFileClip:
   gif_clips = [mp.VideoFileClip(path_to_gif) for path_to_gif in infiles]
@@ -18,8 +19,23 @@ def create_video_from_gifs(infiles, outfile = None, gif_duration = None) -> mp.V
 
   return combined_clip
 
-def get_beat_timestamps(audio):
+def get_beat_timestamps(audio: Audio):
   """..."""
   tempo, beat_frames = librosa.beat.beat_track(y=audio.waveform, sr=audio.samplerate)
   return librosa.frames_to_time(beat_frames, sr=sr)
 
+def attach_audio_to_video(path_to_audio: Audio, video: mp.VideoFileClip, outfile = None):
+  """
+  overlays audio on a moviepy video clip
+  """
+  audio = mp.AudioFileClip(path_to_audio)
+
+  if audio.duration > video.duration:
+    audio.set_duration(video.duration)
+
+  video.audio = mp.afx.audio_loop(audio, duration=video.duration)
+
+  if outfile is not None:
+    video.write_videofile(outfile, codec='libx264', audio_codec='aac')
+
+  return video
