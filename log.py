98490c1daf9213c1d348b7b8939fd72407bcf8e2
perintyler <perintyler@gmail.com>
2023-08-11 12:08:02 -0400

    make log module more usable

--- log.py
+++ log.py
@@ -7,32 +7,24 @@ VERBOSE = True
 
 LOGFILE_NAME = f'./logs/{datetime.now().isoformat()}.json'
 
+__print_logs__ = True
 __logs__ = []
 
-def message(msg):
-  """prints if verbose mode is on, otherwise gets written to log file"""
+def start_printing(): __print_logs__ = False
+def stop_printing(): __print_logs__ = True
+
+def new_message(msg):
+  """prints if verbose mode is on, otherwise gets written to log file
+  """
   __logs__.append(msg)
-  if (VERBOSE):
+  if __print_logs__:
     print(f'  - {msg}')
 
-def header(title):
-  title = ' '.join(word.title() for word in title.split(' '))
-  header = f'<><><> {title} <><><>'
-  indent = ' '*int((128 - len(header))/2 + 1)
-  headerDivider = '-'*128
-  print(f"\n{headerDivider}\n{indent}{header}\n{headerDivider}\n")
-
-def divider():
-  print('\n')
-  print('-'*128)
-  print('/'*64 + '\\'*64)
-  print('\\'*64 + '/'*64)
-  print('-'*128)
-  print('')
-
 def save_all():
-  if VERBOSE: header(f'saving logs to {LOGFILE_NAME}')
-  if not os.path.isdir('logs'): os.mkdir('logs')
+  """saves all messages to a json file
+  """
+  if not os.path.isdir('logs'): 
+    os.mkdir('logs')
+
   with open(LOGFILE_NAME, 'w') as logfile:
     json.dump(__logs__, logfile)
-
