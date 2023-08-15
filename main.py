596942dee1ddd3874d6f0406b2fa61a4f2d608d1
perintyler <perintyler@gmail.com>
2023-08-15 19:00:19 -0400

    implement config file for running the program with cli args

--- main.py
+++ main.py
@@ -5,19 +5,26 @@ import json
 import argparse
 
 from music_video import MusicVideo
-from text_to_image import generate_image
+from text_to_image import text_to_image
+from config import load_config
+
+EXIT_BEFORE_COMPLETING_SEGMENTS = False
+EXIT_BEFORE_COMPILING_SEGMENTS = True
+
 
 def prompt_user_is_ready_to_generate_multimedia() -> bool:
   """give the user a chance to stop the program and verify that lyrics are correct in the segments JSON file
   """
+  if AUTOMATICALLY_EXIT_INBETWEEN_STEPS: return False
   prompt = 'Do you want to check that the lyrics were transcribed correctly before generating images? (y/n)'
   return input(prompt) == 'y'
 
 def prompt_user_is_ready_to_compile_segments() -> bool:
   """give the user a chance to stop the program and verify that each segment's multimedia looks good
   """
+  if AUTOMATICALLY_EXIT_INBETWEEN_STEPS: return False
   prompt = 'All multimedia for the video has been successfully generated. Are you ready to compile and save the final video? (y/n)'
-  return input(prompt) == 'y': 
+  return input(prompt) == 'y'
 
 def main(audio_file, output_directory):
   """Entry point for music video generation
@@ -53,22 +60,25 @@ def main(audio_file, output_directory):
   else:
     # segments file has not been created. Create a new music video 
     # object save the incomplete segments to a JSON file
-    music_video = MusicVideo.create_new(path_to_song)
+    music_video = MusicVideo.create_new(audio_file)
     music_video.save_as_json(segments_file)
 
-  if not prompt_user_is_ready_to_generate_multimedia(): return
+  segments_already_complete = music_video.is_complete()
+
+  if EXIT_BEFORE_COMPILING_SEGMENTS: return
 
   ###
   # STEP 3: generate multimedia for any incomplete segment (i.e. segment is missing multimedia)
   ###
 
-  for segment in music_video.get_incomplete_segments()
-    path_to_multimedia = os.path.join(output_directory, 'multimedia', segment.bar.line_number)
-    generate_image(segment.bar.text, output_file)
+  for segment in music_video.get_incomplete_segments():
+    multimedia_directory = os.path.join(output_directory, 'multimedia', segment.bar.line_number)
+    if not os.path.exists(multimedia_directory): os.mkdir(multimedia_directory)
+    text_to_image(segment.bar.text, multimedia_directory)
     music_video.set_multimedia(line_number, output_file)
     music_video.save_as_json(segments_json_file)
 
-  if not prompt_user_is_ready_to_compile_segments(): return
+  if EXIT_BEFORE_COMPILING_SEGMENTS: return
 
   ###
   # STEP 4: compile the final mp4 file using the now completed segments
@@ -81,5 +91,14 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('--audio-file', required=True)
   parser.add_argument('--output-directory', required=True)
-  args = parser.parse_args()
-  main(args.audio_file, args.output_directory)
+  config = load_config()
+
+  if config:
+    audio_file = config.audio_file
+    output_directory = config.output_directory
+  else:
+    args = parser.parse_args()
+    audio_file = args.audio_file
+    output_directory = args.output_directory
+
+  main(audio_file, output_directory)
