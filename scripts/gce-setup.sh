638bcfa5c11ba41275e713e36b7a84b7181c6928
perintyler <perintyler@gmail.com>
2023-08-23 13:09:15 -0400

    script for setting up environment in GCP VM

--- scripts/gce-setup.sh
+++ scripts/gce-setup.sh
@@ -4,5 +4,7 @@ sudo apt-get install git-all -y
 git clone https://github.com/perintyler/AutoMusicvideo.git
 cd AutoMusicvideo
 
+sudo apt-get update -qq && sudo apt-get install ffmpeg -y
+
 sudo apt-get install python3-pip -y
 python3 -m pip install -r requirements.txt
