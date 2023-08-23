65dd8b48508c9743ac9ecd3c4cdb11db0b2a84f7
perintyler <perintyler@gmail.com>
2023-08-23 13:06:25 -0400

    cloud sync module test shells

--- bucket.py
+++
@@ -1,4 +0,0 @@
-"""bucket.py"""
-
-def upload_image(path_to_image):
-  pass
---
+++ cloud_sync_test.py
@@ -0,0 +1,9 @@
+"""cloud_sync_test.py"""
+
+TEST_BUCKET = 'music-video-test-bucket'
+
+def test_apply_local_updates_to_gcp():
+  pass
+
+def test_apply_gcp_updates_to_local():
+  pass
