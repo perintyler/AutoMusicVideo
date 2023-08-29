1b91f9ec8a04172fb2bbc153ee1841b4ed5e704b
perintyler <perintyler@gmail.com>
2023-08-29 19:50:53 -0400

    image concatenation for scene clip

--- editing_room/editing_room/scene.py
+++ editing_room/editing_room/scene.py
@@ -3,10 +3,15 @@
 from typing import List
 from dataclasses import dataclass
 
-import moviepy.editor as mp
+from PIL import Image
+from numpy import asarray
+from moviepy.editor import ImageClip, concatenate_videoclips
 import storyboard
 
 from .beats import Beats
+from .audio import Audio
+
+FRAMES_PER_SECOND = 24
 
 @dataclass
 class Scene:
@@ -18,7 +23,7 @@ class Scene:
   end_time: float
 
   @property
-  def duration(self):
+  def duration(self) -> float:
     """
     note: this is different than chapter duration, since the scene will start and end in 
     sync with the audio, not neccessarily when the chapter's lyric starts or ends
@@ -30,26 +35,32 @@ class Scene:
     combines the chapter's content (i.e. images) into a video clip, which should show the 
     text to image model thought process as it "draws" to fit the prompt (i.e. lyrics)
     """
-    image_clip_duration = self.duration / chapter.num_content_files()
+    subclip_duration = self.duration / self.chapter.num_content_files()
 
     subclips = []
-    for image_bytes in chapter.generate_content():
-      subclips.append(
-        mp.ImageClip(image_bytes).set_duration(image_clip_duration)
-      )
 
-    return mp.concatenate_videoclips(clips, method="compose")
+    for image_bytes in self.chapter.generate_content():
+      image = Image.open(image_bytes)
+      image_array = asarray(image)
+      image_clip = ImageClip(image_array, duration=subclip_duration)
+      subclips.append(image_clip)
+
+    return concatenate_videoclips(subclips)
 
   @classmethod
-  def create_all(Cls, chapters: storyboard.Chapter, beats: Beats):
+  def create_all(Cls, audio: Audio, chapters: storyboard.Chapter, beats: Beats):
     """
     creates a scene for each chapter where transitions occur in sync with the song  
     """
+    assert len(chapters) > 0
+
     scenes = []
     for chapter in chapters:
       start_time = 0.0 if chapter.number == 1 else scenes[-1].end_time
       end_time = beats.get_closest(chapter.lyric.end_time)
       scenes.append(Cls(chapter, start_time, end_time))
+
+    scenes[-1].end_time = audio.duration # make the last scene end at the end of the video
 
     return scenes
 
