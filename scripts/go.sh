5b78a7dba71a874f38b55a624088ccef8e69838d
perintyler <perintyler@gmail.com>
2023-08-29 03:09:51 -0400

    delete now unused scripts

--- scripts/go.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-REPOPATH=$(dirname "$SCRIPTPATH")
-
-cd $REPOPATH;
-. env/bin/activate;
-clear && printf '\33c\e[3J' && python3 main.py
