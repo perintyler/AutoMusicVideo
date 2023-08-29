09752f7c8567ddbd4b21e5b05a899a3e6085f631
perintyler <perintyler@gmail.com>
2023-08-29 18:44:17 -0400

    expose cloud storage package

--- storyboard/storyboard/__init__.py
+++ storyboard/storyboard/__init__.py
@@ -4,6 +4,10 @@ from .main import write_table_of_contents
 from .main import generate_jobs
 from .main import is_complete
 from .main import do_job
+from .main import get_chapters
 
 from .storyboard import TableOfContents
+from .storyboard import StoryboardChapter as Chapter
 from .job_queue import StoryboardJobQueue as JobQueue
+
+from . import cloud_storage
