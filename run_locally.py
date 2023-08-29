aa7b6e0c61744b5869dc054bcea88bf57d3b889b
perintyler <perintyler@gmail.com>
2023-08-29 03:08:51 -0400

    use audio from input-audio directory as main inputs

--- run_locally.py
+++ run_locally.py
@@ -1,5 +1,19 @@
 
+import os
+import pathlib
 import storyboard
 
+INPUT_AUDIO_DIRECTORY = 'input-audio'
+
+def main():
+  for audio_file in os.listdir(INPUT_AUDIO_DIRECTORY):
+    song_path = pathlib.Path(INPUT_AUDIO_DIRECTORY).joinpath(audio_file)
+    song_id = song_path.stem
+
+    storyboard.write_table_of_contents(song_id, audio_file)
+    
+    for job in storyboard.generate_jobs(song_id):
+      storyboard.do_job(song_id, job)
+
 if __name__ == '__main__':
-  storyboard.main('input-audio/something.mp3', 'something-music-video')
+  main()
