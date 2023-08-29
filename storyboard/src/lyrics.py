39b2aee0ce8e2283e4da71e628308d0a2a470f5c
perintyler <perintyler@gmail.com>
2023-08-28 22:12:11 -0400

    simplify lyric transcription

--- storyboard/src/lyrics.py
+++ storyboard/src/lyrics.py
@@ -10,10 +10,8 @@ from typing import List, Dict
 from dataclasses import dataclass, asdict
 
 @dataclass
-class Bar:
-  """a timestamped line of lyrics
-
-  rename to lyric. get rid of Lyrics class
+class Lyric:
+  """a timestamped lyric from a song
   """
 
   text: str
@@ -30,69 +28,52 @@ class Bar:
     }
 
   def get_duration(self):
-    """returns the amount of time elapsed (in seconds) between the start and end of the bar
+    """returns the amount of time elapsed (in seconds) between the start and end of the lyric
     """
     return self.end_time - self.start_time
 
   @classmethod
-  def from_json(Cls, json_bar):
+  def from_json(Cls, json_lyric):
     """"""
-    text = json_bar['text']
-    line_number = json_bar['line_number'] if 'line_number' in json_bar else json_bar['id']+1
-    start_time = json_bar['start_time'] if 'start_time' in json_bar else json_bar['start']
-    end_time = json_bar['end_time'] if 'end_time' in json_bar else json_bar['end']
-    return Cls(text.strip(), line_number, start_time, end_time)
+    text = json_lyric['text'].strip()
+    line_number = int(json_lyric['line_number'])
+    start_time = float(json_lyric['start_time'])
+    end_time = float(json_lyric['end_time'])
+    return Cls(text, line_number, start_time, end_time)
 
-@dataclass
-class Lyrics:
-  """audio transcriptions with timestamps
-
-  get rid of this class. maybe create a tiny module for saving a list of Lyric instances as srt
-  """
-
-  bars: List[Bar]
-  source_file: str
-
-  def get_plaintext(self) -> str:
-    """returns all lyrics as a single string
-    """ 
-    return '\n'.join(bar.text for bar in self.bars)
-
-  def save_as_srt(self, outfile):
-    """saves each bar and its timestamp and saves it to a SRT file (standard transcription format)
+  @staticmethod
+  def save_as_srt(lyrics: List['Lyric'], outfile=None) -> str:
+    """saves each lyric and its timestamp and saves it to a SRT file (standard transcription format)
     """
-    assert outfile.endswith('.srt')
-
     srt_sections = []
 
-    for bar in self.bars:
-      start_time_string = f'0{timedelta(seconds=int(bar.start_time))},000'
-      end_time_string = f'0{timedelta(seconds=int(bar.end_time))},000'
-      srt_sections.append(f"{bar.line_number}\n{start_time_string} --> {end_time_string}\n{bar.text.strip()}")
+    for lyric in lyrics:
+      start_time_string = f'0{timedelta(seconds=int(lyric.start_time))},000'
+      end_time_string = f'0{timedelta(seconds=int(lyric.end_time))},000'
+      srt_sections.append(f"{lyric.line_number}\n{start_time_string} --> {end_time_string}\n{lyric.text.strip()}")
 
     srt_file_contents = '\n\n'.join(srt_sections)
 
-    with open(outfile, 'w', encoding='utf-8') as srt_file:
-      srt_file.write(srt_file_contents)
+    if outfile is not None:
+      with open(outfile, 'w', encoding='utf-8') as srt_file:
+        assert outfile.endswith('.srt')
+        srt_file.write(srt_file_contents)
 
     return srt_file_contents
 
-  def as_dict(self) -> dict:
-    """serializes all encapsulated data, which can be used to re-create instances of `Lyrics` later on
-    """
-    return {
-      'bars': [bar.as_dict() for bar in self.bars],
-      'source_file': self.source_file, 
-    }
-
   @classmethod
-  def load_from_audio(Cls, filepath):
-    """creates a new instance of `Lyrics` by transcribing lyrics with OpenAI's whisper library 
+  def load_all_from_audio(Cls, filepath) -> List['Lyric']:
+    """transcribes lyrics with OpenAI's whisper library 
     """
     transcription_model = whisper.load_model("base")
-
     transcription = transcription_model.transcribe(audio=filepath, fp16=False)
-    bars = [Bar.from_json(segment) for segment in transcription['segments']]
 
-    return Cls(bars, filepath)
+    lyrics = []
+    for segment in transcription['segments']:
+      text = segment['text'].strip()
+      line_number = int(segment['id'])+1
+      start_time = float(segment['start'])
+      end_time = float(segment['end'])
+      lyrics.append(Cls(text, line_number, start_time, end_time))
 
+    return lyrics
--- storyboard/src/lyrics_test.py
+++ storyboard/src/lyrics_test.py
@@ -1,28 +1,23 @@
 """subtitles_test.py"""
 
 import pathlib
+from importlib.resources import files
 import os
-from .lyrics import Lyrics
+from .lyrics import Lyric
 
-lyrics1 = Lyrics.load_from_audio('test-data/audio/something-beatles_verse1.wav')
+path_to_test_audio = files('storyboard.data').joinpath('something-beatles_verse1.mp3')
+test_lyrics = Lyric.load_all_from_audio(str(path_to_test_audio))
 
-def test_get_bars():
-  assert [bar.text for bar in lyrics1.bars] == [
+def test_load_lyrics_from_audio():
+  assert [lyric.text for lyric in test_lyrics] == [
     "Something in the way she moves attracts me like no other lover",
     "Something in the way she moves me"
   ]
-
-def test_get_lyrics():
-  assert lyrics1.get_plaintext() == \
-"""Something in the way she moves attracts me like no other lover
-Something in the way she moves me"""
+  assert [lyric.line_number for lyric in test_lyrics] == [1, 2]
 
 def test_srt_file_generation():
   srt_filepath = '/tmp/subtitle-test.srt'
-  lyrics1.save_as_srt(outfile=srt_filepath)
-  assert os.path.exists(srt_filepath)
-  with open(srt_filepath) as srt_file:
-    assert srt_file.read() == \
+  expected = \
 """1
 00:00:00,000 --> 00:00:19,000
 Something in the way she moves attracts me like no other lover
@@ -31,3 +26,7 @@ Something in the way she moves attracts me like no other lover
 00:00:19,000 --> 00:00:26,000
 Something in the way she moves me"""
 
+  assert Lyric.save_as_srt(test_lyrics, outfile=srt_filepath) == expected
+  assert os.path.exists(srt_filepath)
+  with open(srt_filepath) as srt_file:
+    assert srt_file.read() == expected
