96071219a0575bdaf9267708296f9b3573c62a56
perintyler <perintyler@gmail.com>
2023-08-28 23:45:38 -0400

    expose storyboard functions

--- storyboard/src/__init__.py
+++ storyboard/src/__init__.py
@@ -1,1 +1,11 @@
-from .main import main
+"""__init__.py"""
+
+from .main import start_new_storyboard as start_new
+from .main import storyboard_exists as exists
+from .main import storyboard_is_done as is_done
+from .main import has_storyboard_work as has_work
+from .main import complete_next_storyboard_job as complete_next_job
+from .main import get_storyboard_song_ids as get_song_ids
+
+from .main import generate_incomplete_jobs
+from .main import do_job
