4ed0fc42b82b80ed862cc6ecf00d7d9fcf87cd76
perintyler <perintyler@gmail.com>
2023-08-29 18:47:49 -0400

    test scene class

---
+++ editing_room/editing_room/scene_test.py
@@ -0,0 +1,17 @@
+"""Test File: AutoMusicVideo::editing_room.scene_test"""
+
+from importlib.resources import files
+from .audio import Audio
+from .beats import Beats
+
+def test_scene():
+  audio_file = files('editing_room.test_audio').joinpath('piano-clip.mp3')
+
+  def test_create_scenes():
+    pass
+
+  def test_scene_duration():
+    pass
+
+  def test_scene_as_clip():
+    pass
