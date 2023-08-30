aac812c02049854acafaab0eb7877da54ccbf2ab
perintyler <perintyler@gmail.com>
2023-08-29 21:07:53 -0400

    music video test shells

---
+++ editing_room/editing_room/music_video_test.py
@@ -0,0 +1,10 @@
+"""Test File: AutoMusicVideo::editing_room.music_video"""
+
+def test_create_music_video():
+  pass
+
+def test_music_video_as_clip():
+  pass
+
+def test_music_video_as_bytes():
+  pass
