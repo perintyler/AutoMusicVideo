2c0936af3419cb8a0fa5c17087e3bae723711497
perintyler <perintyler@gmail.com>
2023-09-19 17:00:32 -0400

    add overlay_lyrics optional arg to Scene::as_clip

--- editing_room/editing_room/scene.py
+++ editing_room/editing_room/scene.py
@@ -13,19 +13,7 @@ from .audio import Audio
 
 FRAMES_PER_SECOND = 24
 
-OVERLAY_LYRICS = True
-
-SAVE_SCENES_LOCALLY = True
-
-KAREOKE = False
-
 LYRICS_POSITION = ("center", "bottom")
-
-def get_karaoke_clip(video_clip, lyrics):
-  lyrics_clip = TextClip(lyrics, fontsize=24, color="gray")
-  lyrics_clip.set_duration(video_clip.duration)
-  lyrics_clip = lyrics_clip.set_position(LYRICS_POSITION)
-  return CompositeVideoClip([video_clip, lyrics_clip])
 
 @dataclass
 class Scene:
@@ -44,7 +32,7 @@ class Scene:
     """
     return self.end_time - self.start_time
 
-  def as_clip(self):
+  def as_clip(self, overlay_lyrics = True):
     """
     combines the chapter's content (i.e. images) into a video clip, which should show the 
     text to image model thought process as it "draws" to fit the prompt (i.e. lyrics)
@@ -61,14 +49,11 @@ class Scene:
 
     scene_clip = concatenate_videoclips(subclips)
 
-    if KAREOKE:
-      scene_clip = get_karaoke_clip(scene_clip, self.chapter.lyric.text)
-
-    if SAVE_SCENES_LOCALLY:
-      kareoke_clip.write_videofile(
-        f'scene-{self.chapter.number}.mp4', 
-        codec='libx264', audio_codec='aac', fps=FRAMES_PER_SECOND
-      )
+    if overlay_lyrics:
+      lyrics_clip = TextClip(lyric, fontsize=24, color="gray")
+      lyrics_clip.set_duration(duration)
+      lyrics_clip = lyrics_clip.set_position(LYRICS_POSITION)
+      scene_clip = CompositeVideoClip([scene_clip, lyrics_clip])
 
     return kareoke_clip
 
@@ -88,15 +73,3 @@ class Scene:
     scenes[-1].end_time = audio.duration # make the last scene end at the end of the video
 
     return scenes
-
-    # for beat_index in range(len(beat_timestamps-1)):
-    #   early_beat_timestamp = beat_timestamps[beat_index]
-    #   late_beat_timestamp = beat_timestamps[beat_index+1]
-    #   scene_index = len(scenes)
-    #   chapter_end_time = chapters[scene_index].end_time
-    #   if chapter_end_time > early_beat_timestamp and chapter_end_time < late_beat_timestamp:
-    #     time_since_last_beat = chapter_end_time - beat_timestamps[beat_index]
-    #     time_until_next_beat = late_beat_timestamp - chapter_end_time
-    #     transition_timestamp = early_beat_timestamp if time_since_last_beat < time_until_next_beat else late_beat_timestamp
-    #     previous_transition_timestamp = scenes[-1].end_time if len(scenes) > 0 else 0.0
-    #     scenes.append(Cls(chapter, previous_transition_timestamp, transition_timestamp))
