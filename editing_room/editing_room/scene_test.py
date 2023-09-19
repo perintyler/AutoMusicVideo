915e70a07e7b3de9789ab49fe6d9ce795dc689cf
perintyler <perintyler@gmail.com>
2023-09-19 17:00:51 -0400

    test for creating all scenes

--- editing_room/editing_room/scene_test.py
+++ editing_room/editing_room/scene_test.py
@@ -4,8 +4,23 @@ from importlib.resources import files
 from .audio import Audio
 from .beats import Beats
 
+scenes = []
+
+def test_create_all_scenes():
+  audio = Audio.load(files('editing_room.test_audio').joinpath('piano-clip.mp3'))
+  chapters = None
+  beats = None
+
+  for chapter, scene in Scene.create_all(audio, chapters, beats):
+    assert scene.duration > 0
+    assert scene.chapter == chapter
+    scenes.append(scene)
+
+def test_scene_as_clip():
+  pass
+
 def test_scene():
-  audio_file = files('editing_room.test_audio').joinpath('piano-clip.mp3')
+  
 
   def test_create_scenes():
     pass
