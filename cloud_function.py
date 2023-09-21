e3613f0f3f35d2b2019eb8ae4163f483f3c72023
perintyler <perintyler@gmail.com>
2023-09-21 16:20:15 -0400

    cloud_function decorator

--- cloud_function.py
+++ cloud_function.py
@@ -22,3 +22,20 @@ def entrypoint(f):
   """
   """
   return modal.Stub(MODAL_PROJECT_NAME).local_entrypoint(f)
+
+def cloud_function(gpu=None, timeout=None):
+  """
+  """
+  local_entry_point = modal.Stub(MODAL_PROJECT_NAME).local_entrypoint(local_entrypoint)
+
+  file_filter = lambda filepath: not filepath.endswith('__pycache__') and not filepath.endswith('.pyc')
+
+  file_mount = modal.Mount.from_local_dir('storyboard', condition=file_filter) \
+                          .add_local_dir('input-audio')
+
+  return modal.Stub(MODAL_PROJECT_NAME).function(
+    gpu     = gpu,
+    image   = modal.Image.from_dockerfile(DOCKERFILE_PATH, context_mount=file_mount), 
+    secret  = modal.Secret.from_name(MODAL_SECRETS_ID),
+    timeout = timeout
+  )
